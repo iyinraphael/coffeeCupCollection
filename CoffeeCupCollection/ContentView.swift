a07563da47283afa051db5acd33bf037b9c6a801
Iyin Raphael <iyinraphael@gmail.com>
2023-07-11 22:26:53 -0400

    create resuable image view

--- CoffeeCupCollection/ContentView.swift
+++ CoffeeCupCollection/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                CupImageView(imageName: "texas")
+                    .padding()
+
+                CupImageView(imageName: "korea")
+            }
+            HStack {
+                CupImageView(imageName: "ibadan")
+                    .padding()
+
+                CupImageView(imageName: "raleigh")
+            }
+            HStack {
+                CupImageView(imageName: "seatle")
+                    .padding()
+                CupImageView(imageName: "peurto-rico")
+            }
         }
         .padding()
     }
@@ -22,5 +35,15 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CupImageView: View {
+    let imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .frame(width: 150, height: 200)
     }
 }
