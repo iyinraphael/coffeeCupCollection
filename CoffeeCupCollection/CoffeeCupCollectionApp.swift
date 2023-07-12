31bb36b5283447cc56d9108264984bc5ba5dda55
Iyin Raphael <iyinraphael@gmail.com>
2023-07-11 20:16:24 -0400

    inital commit

---
+++ CoffeeCupCollection/CoffeeCupCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeCupCollectionApp.swift
+//  CoffeeCupCollection
+//
+//  Created by Iyin Raphael on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeCupCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
